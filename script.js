652dc42e19e701086ee789b48ea8f565166358ae
feldaleks <feldaleks@gmail.com>
2023-07-04 03:54:16 +0300

    add removeCommit

--- script.js
+++ script.js
@@ -46,8 +46,8 @@ const Tweeter = function () {
     }
 
     const removeComment = (postId, CommentId) => {
-        let post = posts[posts.id == postId]
-        post.comments = post.commets.filter(obj => obj.id !== CommentId)
+        let post = posts.filter(obj => obj.id == postId)
+        post[0].comments = post[0].comments.filter(obj => obj.id !== CommentId)
     }
 
     return {
@@ -65,11 +65,33 @@ const tweeter = Tweeter()
 
 tweeter.addPost("This is my own post!")
 console.log(tweeter.getPosts())
+//This should be added to the posts array:
+//{text: "This is my own post!", id: "p3", comments: []}
 
 tweeter.removePost("p1")
 console.log(tweeter.getPosts())
+//There should only be two posts in the post's array:
+//{text: "Aw man, I wanted to be first", id: "p2", comments: Array(3)}
+//{text: "This is my own post!", id: "p3", comments: []}
+
+//============================
+//============================
+//Stop here
+//Make sure everything works. Then keep going
+//============================
+//============================
 
 tweeter.addComment("Damn straight it is!", "p3")
 tweeter.addComment("Second the best!", "p2")
 console.log(tweeter.getPosts())
+//This should be added to the third post's comments array:
+//{id: "c7", text: "Damn straight it is!"}
+
+//This should be added to the second post's comments array:
+//{id: "c8", text: "Second the best!"}
+
+tweeter.removeComment("p2", "c6")
+console.log(tweeter.getPosts())
+//This comment should be removed:
+//{id: "c6", text: "Haha second place what a joke."}
 
